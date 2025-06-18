f3d754b0d52956888a676697716123a95a9a54bc
Shared Ordaz <sharedordaz@live.com.mx>
2025-06-17 21:06:15 -0600

    .bat script

--- runWindows.bat
+++ runWindows.bat
@@ -1,7 +1,25 @@
 @echo off
+echo Checking if pnpm is installed...
 
-:: Abrir backend en nueva ventana
+where pnpm >nul 2>nul
+if %errorlevel% neq 0 goto install_pnpm
+
+echo Pnpm is already installed.
+goto StartApp
+
+:install_pnpm
+echo Pnpm is not installed. Installing now...
+npm install -g @pnpm/exe@latest-10
+echo Pnpm installation script executed.
+start "" cmd /k "runWindows.bat"
+pause
+exit /b
+
+:StartApp
+echo Starting the application...
+REM Abrir terminal de backend y frontend en nuevas ventanas
+echo Starting backend and frontend in new terminal windows...
 start "Backend" cmd /k "cd backend && npm install && npm start"
-
-:: Abrir frontend en nueva ventana
 start "Frontend" cmd /k "cd frontend && pnpm install && pnpm start"
+pause
+exit /b
