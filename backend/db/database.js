7183dd16ce2d984aede5e2a748ca2f64e3b2f5e9
desivar <jillian_desire@yahoo.es>
2025-06-17 18:12:39 -0600

    fixed typo

--- backend/db/database.js
+++ backend/db/database.js
@@ -11,8 +11,8 @@ const connectDB = async () => {
     return _db;
   }
 
-  const uri = process.env.MONGODB_URL;
-  if (!uri) throw new Error('MONGODB_URL is not defined.');
+  const url = process.env.MONGODB_URL;
+  if (!url) throw new Error('MONGODB_URL is not defined.');
 
   const client = new MongoClient(url);
   await client.connect();
